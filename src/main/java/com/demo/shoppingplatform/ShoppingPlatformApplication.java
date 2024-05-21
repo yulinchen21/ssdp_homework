330b65571efb49b09123ac9b8e70cdee23e24090
Chen Yulin <chern.yulin@gmail.com>
2024-05-21 23:23:45 +0800

    feat: init repo

---
+++ src/main/java/com/demo/shoppingplatform/ShoppingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.demo.shoppingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingPlatformApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingPlatformApplication.class, args);
+  }
+
+}
