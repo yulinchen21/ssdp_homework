21f1f9219aca709312a29b35f70b3bd065b17dba
Chen Yulin <chern.yulin@gmail.com>
2024-05-23 13:31:30 +0800

    feat: add getter setter for Product

--- src/main/java/com/demo/shoppingplatform/model/Product.java
+++ src/main/java/com/demo/shoppingplatform/model/Product.java
@@ -14,4 +14,44 @@ public class Product {
   private String imageUrl;
   private double price;
   private int stock;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(final Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(final String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(final double price) {
+    this.price = price;
+  }
+
+  public int getStock() {
+    return stock;
+  }
+
+  public void setStock(final int stock) {
+    this.stock = stock;
+  }
 }
