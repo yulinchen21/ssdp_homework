9953987a991a967144d96292f6c6fb8db55b6069
Chen Yulin <chern.yulin@gmail.com>
2024-06-04 23:01:22 +0800

    feat(DESIGN_DAY5): fetch data from back end and display them

--- Day_05_design/shopping-platform-ui/src/app/product-list/product-list.component.ts
+++ Day_05_design/shopping-platform-ui/src/app/product-list/product-list.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable
+} from "@angular/material/table";
+import {Product} from "../../interface/Product";
+import {ProductListService} from "../../service/product-list.service";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRowDef,
+    MatRowDef
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  dataSource: Product[] = [];
+  displayedColumns = ['id', 'name', 'price', 'stock'];
 
+  constructor(private productListService: ProductListService) {}
+
+  ngOnInit(): void {
+    this.productListService.getProducts(1,15).subscribe(res => this.dataSource = res);
+  }
 }
